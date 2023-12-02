205676e99ea5ac6c1667ba0964478182ba81138b
lucas <lucaotaveira@gmail.com>
2023-12-02 15:28:33 -0300

    Atualização dashboard

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 #importação das bibliotecas
 from flask import Flask, request, render_template
 import mysql.connector
+import pandas as pd
+
+from class_graficos import Graficos
 
 #inicia o flask
 app = Flask(__name__)
-
 
 # Conecta com mysql
 conexao = mysql.connector.connect(
@@ -13,9 +15,14 @@ conexao = mysql.connector.connect(
     password='root',
     database = 'atividade' )
 
-# Rota do index
+
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+# Rota do registro doenca
+@app.route('/registro_doenca', methods=['POST','GET'])
+def registro_doenca():
     if request.method == 'POST':
         diagnostico = request.form['diagnostico']
         eritrocitos = request.form['eritrocitos']
@@ -39,8 +46,43 @@ def index():
         basofilos = request.form['basofilos']
         plaquetas = request.form['plaquetas']
 
-    return render_template('index.html')
+        cursor = conexao.cursor()
 
+        comando_insert = f"INSERT INTO formulario (diagnostico, eritrocitos, hemoglobina, dematocrito, hcm, vgm, chgm, metarrubricitos, proteina_plasmatica, leucocitos, leucograma, segmentados, bastonetes, blastos, metamielocitos, mielocitos, linfocitos, monocitos, eosinofilos, basofilos, plaquetas)VALUES ('{diagnostico}', '{eritrocitos}', '{hemoglobina}', '{dematocrito}', '{hcm}', '{vgm}', '{chgm}', '{metarrubricitos}', '{proteina_plasmatica}', '{leucocitos}', '{leucograma}', '{segmentados}', '{bastonetes}', '{blastos}', '{metamielocitos}', '{mielocitos}', '{linfocitos}', '{monocitos}', '{eosinofilos}', '{basofilos}', '{plaquetas}');"
+
+        cursor.execute(comando_insert)
+        conexao.commit()
+        cursor.close()
+
+    return render_template('registro_doenca.html')
+
+
+def fetch_data():
+    # Consultar dados do MySQL
+    query = "SELECT * FROM formulario;"
+    df = pd.read_sql(query, conexao)
+    return df
+
+@app.route('/dashboard', methods=['POST','GET'])
+def dashboard():
+    if request.method == 'POST':
+        # Obter escolhas do usuário do formulário
+        tipo_grafico = request.form['grafico']
+        dado1 = request.form['dado1']
+        dado2 = request.form['dado2']
+
+        # Obter gráfico com base nas escolhas do usuário
+        df = fetch_data()  # Certifique-se de ter a função fetch_data() definida
+        gf = Graficos(df)
+
+        dado1 = None if dado1 == 'null' else dado1
+        dado2 = None if dado2 == 'null' else dado2
+
+        graph = gf.gerar_grafico(tipo_grafico, dado1, dado2)
+
+        return render_template('dashboard.html', graph=graph)
+
+    return render_template('dashboard.html')
 
 #executa o aplicativo flask
 if __name__ == '__main__':
---
+++ class_graficos.py
@@ -0,0 +1,212 @@
+from io import BytesIO
+import base64
+import plotly.express as px
+import plotly.graph_objects as go
+
+class Graficos:
+    def __init__(self, df):
+        """
+        Inicializa a classe com um DataFrame.
+        """
+        self.df = df
+
+    def _salvar_grafico(self, fig):
+        """
+        Salva o gráfico em um formato de imagem base64.
+        """
+        img_buffer = BytesIO()
+        fig.write_image(img_buffer, format='png')
+        img_buffer.seek(0)
+        img_str = base64.b64encode(img_buffer.read()).decode('utf-8')
+        return img_str
+ 
+
+    def gerar_grafico(self, tipo_grafico, dado1, dado2):
+        """
+        Gera um gráfico com base no tipo e nos dados fornecidos.
+        """
+        graficos_disponiveis = {
+            'barras': self.gerar_grafico_barra,
+            'pizza': self.gerar_grafico_pizza, #1
+            'dispersao': self.gerar_grafico_dispersao,
+            'linha': self.gerar_grafico_linha,
+            'area': self.gerar_grafico_area,
+            'radar': self.gerar_grafico_radar, #1
+            'area_empilhada': self.gerar_grafico_area_empilhada,
+            'barras_empilhadas': self.gerar_grafico_barras_empilhadas
+            # Adicione outras entradas conforme necessário
+        }
+
+        if dado1 is None and dado2 is None:
+            return 'Nenhum dado selecionado'
+
+        if tipo_grafico in graficos_disponiveis:
+            if tipo_grafico == 'pizza' or tipo_grafico == 'radar':
+                graph = graficos_disponiveis[tipo_grafico](dado1)
+            else:
+                graph = graficos_disponiveis[tipo_grafico](dado1, dado2)
+        else:
+            # Lógica para lidar com um tipo de gráfico desconhecido, se necessário
+            graph = None
+
+        return graph 
+
+    def gerar_grafico_barra(self, x=None, y=None):
+        """
+        Gera um gráfico de barras.
+        """
+
+        if x is not None and y is not None:
+           title = f'Gráfico de Barras - {y} por {x}'
+           labels = {x: x, y:y}
+        elif x is not None and y is None:
+            title = f'Gráfico de Barras - Contagem por {x}'
+            labels = {x: x, 'count': 'Contagem'}
+        else:
+            title = f'Gráfico de Barras - {y} por Contagem'
+            labels = {'count': 'Contagem',y:y}
+
+        fig = px.bar(self.df, x=x, y=y, title=title, labels=labels)
+        return self._salvar_grafico(fig)
+
+    def gerar_grafico_pizza(self, dado):
+        """
+        Gera um gráfico de pizza.
+        """
+        fig = px.pie(self.df, names=dado, title=f'Gráfico de Pizza - Distribuição por {dado}')
+
+        fig.update_traces(textinfo='percent+label', pull=[0.05] * len(self.df[dado]),
+                          showlegend=False, selector=dict(type='pie'))
+
+        return self._salvar_grafico(fig)
+    
+    def gerar_grafico_dispersao(self, x=None, y=None):
+        """
+        Gera um gráfico de dispersão.
+        """
+
+        if x is not None and y is not None:
+           title = f'Gráfico de Dispersão - {y} em função de {x}'
+           labels = {x: x, y:y}
+        elif x is not None and y is None:
+            title = f'Gráfico de Dispersão - Contagem por {x}'
+            labels = {x: x, 'count': 'Contagem'}
+        else:
+            title = f'Gráfico de Dispersão - {y} por Contagem'
+            labels = {'count': 'Contagem',y:y}
+
+        fig = px.scatter(self.df, x=x, y=y, title=title, labels=labels)
+        return self._salvar_grafico(fig)
+
+    def gerar_grafico_linha(self, x=None, y=None):
+        """
+        Gera um gráfico de linha.
+        """
+
+        if x is not None and y is not None:
+           title = f'Gráfico de Linha - {y} em função de {x}'
+           labels = {x: x, y:y}
+        elif x is not None and y is None:
+            title = f'Gráfico de Linha - Contagem por {x}'
+            labels = {x: x, 'count': 'Contagem'}
+        else:
+            title = f'Gráfico de Linha - {y} por Contagem'
+            labels = {'count': 'Contagem',y:y}
+
+        #title = f'Gráfico de Linha - {y} em função de {x}' if y else f'Gráfico de Linha - {x} por contagem'
+        #labels = {'count': 'Contagem', x: x} if not y else {x: x, y: y}
+
+        fig = px.line(self.df, x=x, y=y, title=title, labels=labels)
+        return self._salvar_grafico(fig)
+    
+    def gerar_grafico_area(self, x = None, y=None):
+        """
+        Gera um gráfico de área.
+        """
+
+        if x is not None and y is not None:
+           title = f'Gráfico de Área - {y} em função de {x}'
+           labels = {x: x, y:y}
+        elif x is not None and y is None:
+            title = f'Gráfico de Área - Contagem por {x}'
+            labels = {x: x, 'count': 'Contagem'}
+        else:
+            title = f'Gráfico de Área - {y} por Contagem'
+            labels = {'count': 'Contagem',y:y}
+
+        #title = f'Gráfico de Área - {y} por {x}' if y else f'Gráfico de Área - {x} por contagem'
+        #labels = {'count': 'Contagem', x: x} if not y else {x: x, y: y}
+
+        fig = px.area(self.df, x=x, y=y, title=title, labels=labels)
+        return self._salvar_grafico(fig)
+
+    def gerar_grafico_radar(self):
+        """
+        Gera um gráfico de radar.
+        """
+        colunas_numericas = self.df.select_dtypes(include=['number']).columns
+        fig = go.Figure()
+
+        for i, (_, row) in enumerate(self.df.iterrows()):
+            fig.add_trace(go.Scatterpolar(
+                r=row[colunas_numericas].values,
+                theta=colunas_numericas,
+                fill='toself',
+                name=str(i)  # Use a contagem numérica como nome
+            ))
+
+        fig.update_layout(
+            polar=dict(
+                radialaxis=dict(
+                    visible=True,
+                    range=[0, self.df[colunas_numericas].max().max()]
+                )
+            ),
+            showlegend=True,
+            title='Gráfico de Radar'
+        )
+
+        return self._salvar_grafico(fig)
+    
+    def gerar_grafico_area_empilhada(self, x=None, y=None):
+        """
+        Gera um gráfico de área empilhada.
+        """
+        
+        if x is not None and y is not None:
+           title = f'Gráfico de Área Empilhada - {y} em função de {x}'
+           labels = {x: x, y:y}
+        elif x is not None and y is None:
+            title = f'Gráfico de Área Empilhada - Contagem por {x}'
+            labels = {x: x, 'count': 'Contagem'}
+        else:
+            title = f'Gráfico de Área Empilhada - {y} por Contagem'
+            labels = {'count': 'Contagem',y:y}
+
+        #title = f'Gráfico de Área Empilhada - {x} por {y}'
+        #labels = {'count': 'Contagem', x: x}
+
+        fig = px.area(self.df, x=x, title=title, labels=labels, color=y)
+        return self._salvar_grafico(fig)
+    
+    def gerar_grafico_barras_empilhadas(self, x=None, y=None):
+        """
+        Gera um gráfico de barras empilhadas.
+        """
+
+        if x is not None and y is not None:
+           title = f'Gráfico de Barras Empilhadas - {y} em função de {x}'
+           labels = {x: x, y:y}
+        elif x is not None and y is None:
+            title = f'Gráfico de Barras Empilhadas - Contagem por {x}'
+            labels = {x: x, 'count': 'Contagem'}
+        else:
+            title = f'Gráfico de Barras Empilhadas - {y} por Contagem'
+            labels = {'count': 'Contagem',y:y}
+
+        
+        #title = f'Gráfico de Barras Empilhadas - {x} por {y}'
+        #labels = {x: x, 'count': 'Contagem'}
+
+        fig = px.bar(self.df, x=x, title=title, labels=labels, color=y)
+        return self._salvar_grafico(fig)
